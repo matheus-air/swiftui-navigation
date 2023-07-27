4b7d9a52ef4784b80deb9fbc9003d33b1b7381b3
Matheus Bispo <matheus.bispo@airapps.com>
2023-07-27 14:50:17 -0300

    feat: update iOS version compatibility to v11 for dependencies

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
   name: "swiftui-navigation",
   platforms: [
-    .iOS(.v13),
+    .iOS(.v11),
     .macOS(.v10_15),
     .tvOS(.v13),
     .watchOS(.v6),
@@ -22,9 +22,9 @@ let package = Package(
   ],
   dependencies: [
     .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
-    .package(url: "https://github.com/pointfreeco/swift-case-paths", from: "0.11.0"),
-    .package(url: "https://github.com/pointfreeco/swift-custom-dump", from: "0.6.0"),
-    .package(url: "https://github.com/pointfreeco/xctest-dynamic-overlay", from: "0.8.0"),
+    .package(url: "git@github.com:matheus-air/swift-case-paths", branch: "ios-11-compatibility"),
+    .package(url: "git@github.com:matheus-air/swift-custom-dump", branch: "ios-11-compatibility"),
+    .package(url: "git@github.com:matheus-air/xctest-dynamic-overlay", branch: "ios-11-compatibility"),
   ],
   targets: [
     .target(
